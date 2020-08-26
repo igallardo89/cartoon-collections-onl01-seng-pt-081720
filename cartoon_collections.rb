8d624900beaf669b2c0ac4915917973ced0c5ba3
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-26 20:06:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,11 +34,7 @@ soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.find do  |x|
-  cheese_include?(x)
-  return
-else
 end 
-end 
-
+end
 
 
